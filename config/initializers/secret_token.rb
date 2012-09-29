a34aa6c3b21a665e80da26c8841cb12144fb67ab
Troy Shu <tmshu1@gmail.com>
2012-09-29 14:28:12 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hacknyf2012::Application.config.secret_token = '786fd6baf9ae8611e832e2280d221c67526068cef1584ce404868362357b57f0a0dbed72666810267ed089409de147b837b7dd20b30b1edd807f37f7f445efc4'
